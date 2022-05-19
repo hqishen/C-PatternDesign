83a55dd7a6cd985036f8630be3ddd44725a6f4c2
Sundae <hqishen@126.com>
2022-05-19 22:11:21 +0800

    添加开闭原则和依赖倒置原则实例化

---
+++ 01.开闭原则/01.开闭原则.cpp
@@ -0,0 +1,75 @@
+﻿// 01.开闭原则.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/*
+传统的写法，一个银行业务员的工作
+如之前只做存款、转账，如果要加其他功能的话，就需要修改这个类，就不符合开闭原则
+*/
+class BanckWorker
+{
+public:
+	BanckWorker();
+	~BanckWorker();
+
+	void save() {
+		std::cout << "存款" << std::endl;
+	}
+
+	void transform() {
+		std::cout << "转账.." << std::endl;
+	}
+
+	// 新的业务,需要添加代码
+	void fund() {
+		std::cout << "基金" << std::endl;
+	}
+
+private:
+};
+
+/*
+开闭原则的写法...
+*/
+/*
+有纯虚函数，属于接口类，派生类需要实现纯虚函数
+*/
+class AbstractBanker {
+public:
+	virtual void business() = 0;
+private:
+};
+/*
+* 派生类
+*/
+class TransformBanker : public AbstractBanker {
+public:
+	virtual void business() {
+		std::cout << "转账" << std::endl;
+	}
+private:
+};
+
+class SaveBanker : public AbstractBanker {
+public:
+	virtual void business() {
+		std::cout << "存钱业务" << std::endl;
+	}
+private:
+};
+
+
+
+int main()
+{
+	AbstractBanker *banker = NULL;
+	banker = new TransformBanker();
+	banker->business();
+	delete banker;
+
+	banker = new SaveBanker();
+	banker->business();
+	delete banker;
+	return 0;
+}
---
+++ 02.依赖倒置原则/02.依赖倒置原则.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+class HardDisk {
+public:
+    virtual void work() = 0;
+};
+
+class Cpu {
+public:
+    virtual void work() = 0;
+};
+
+class Memory {
+public:
+    virtual void work() = 0;
+};
+
+class Computer {
+public:
+    Computer(HardDisk* hd, Memory* mem, Cpu* cpu) 
+        : m_cpu(cpu), m_hd(hd), m_mem(mem) {
+    }
+
+    void work() {
+        m_hd->work();
+        m_cpu->work();
+        m_mem->work();
+    }
+private:
+    HardDisk* m_hd;
+    Cpu* m_cpu;
+    Memory* m_mem;
+};
+
+class AMDCPU : public Cpu {
+public:
+    void work() {
+        std::cout << "Amd cpu and in working order..." << std::endl;
+    }
+};
+
+class WDHD : public HardDisk {
+public:
+    void work() {
+        std::cout << "WD hard disk, in working order..." << std::endl;
+    }
+};
+
+class WGMem : public Memory {
+public:
+    void work() {
+        std::cout << "WG memroy , in working order..." << std::endl;
+    }
+};
+
+
+int main()
+{
+    HardDisk * hd = new WDHD();
+    Memory* mem = new WGMem();
+    Cpu* cpu = new AMDCPU();
+
+    Computer* acer = new Computer(hd, mem, cpu);
+    acer->work();
+    delete acer;
+
+    Computer* lenove = new Computer(hd, mem, cpu);
+    lenove->work();
+
+    delete lenove;
+    delete hd;
+    delete mem;
+    delete cpu;
+    return 0;
+}
+
