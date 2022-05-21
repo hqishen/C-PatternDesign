d8fc2e87cc9c085f141ea2a995f5ebd9a47fd3fd
Sundae <hqishen@126.com>
2022-05-21 09:00:27 +0800

    简单的工厂模式

---
+++ 04.简单工厂模式/04.简单工厂模式.cpp
@@ -0,0 +1,59 @@
+﻿
+#include <iostream>
+
+/*
+不符合开闭原则，不是标准的设计模式
+*/
+class AbstractShape {
+public:
+    virtual void area() = 0;
+};
+
+class Triangle : public AbstractShape {
+public:
+    Triangle(int h = 10, int w = 20) : m_h(h), m_w(w) {
+    }
+
+    void area() {
+        std::cout << "area:" << m_h * m_w / 2 << std::endl;
+    }
+
+private:
+    int m_h;
+    int m_w;
+};
+
+class Round : public AbstractShape {
+public:
+    Round(int r=3) :m_r(r) {
+
+    }
+    void area() {
+        std::cout << "round area:" << m_r * m_r / 2 << std::endl;
+    }
+private:
+    int m_r;
+};
+
+class Factory {
+public:
+    AbstractShape* createShape(int i) {
+        if (i == 0) {
+            return new Triangle();
+        }
+        else if (i == 1) {
+            return new Round();
+        }
+    }
+};
+
+int main()
+{
+    Factory* factory = new Factory();
+    AbstractShape* shape = factory->createShape(0);
+    shape->area();
+    delete shape;
+    shape = factory->createShape(1);
+    shape->area();
+    delete shape;
+}
